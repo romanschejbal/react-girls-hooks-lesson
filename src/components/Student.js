6e8d85206a9b2a3916799f5165dbdd36cc2001be
Roman Schejbal <schejbalroman@gmail.com>
2021-11-25 20:04:40 +0100

    useEffect example

--- src/components/Student.js
+++ src/components/Student.js
@@ -1,23 +1,33 @@
-import React, {useState} from 'react';
+import React, {useState, useEffect} from 'react';
 
 const Student = ({ isThisFemale, firstName, lastName, house, initials }) => {
     const [jabbed, setJabbed] = useState(false);
+    const [displayHouse, setDisplayHouse] = useState(false);
 
     const genderClassName = isThisFemale ? "item girls" : "item";
     const jabbedClassName = jabbed ? 'jabbed' : '';
 
     function onClickJabbed(event) {
-        setJabbed(true);
+        setJabbed(jabbed + 1);
     }
+
+    useEffect(() => {
+        if (jabbed > 0 && !displayHouse) {
+            setTimeout(() => {
+                console.log('HELLO', new Date())
+                setDisplayHouse(true);
+            }, 3000);
+        }
+    }, [jabbed, displayHouse])
 
     return (
         <li className={genderClassName + ' ' + jabbedClassName}>
             <div className='name'>
-                {!jabbed && <button onClick={onClickJabbed}>jab</button>}
+                {jabbed < 2 && <button onClick={onClickJabbed}>jab</button>}
                 <div className='initials'>{initials}</div>
                 <div>{firstName} {lastName}</div>
             </div>
-            <div>{house}</div>
+            {displayHouse > 0 &&<div>{house}</div>}
         </li>
     )
 }
