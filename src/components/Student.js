39091fb088c9015d9274e53e0f53fdf0fe1af349
Roman Schejbal <schejbalroman@gmail.com>
2021-11-25 19:41:32 +0100

    Convert input checkbox to button

--- src/components/Student.js
+++ src/components/Student.js
@@ -6,15 +6,14 @@ const Student = ({ isThisFemale, firstName, lastName, house, initials }) => {
     const genderClassName = isThisFemale ? "item girls" : "item";
     const jabbedClassName = jabbed ? 'jabbed' : '';
 
-    function onChangeJabbed(event) {
-        const jeOckovany = event.target.checked;
-        setJabbed(jeOckovany);
+    function onClickJabbed(event) {
+        setJabbed(true);
     }
 
     return (
         <li className={genderClassName + ' ' + jabbedClassName}>
             <div className='name'>
-                <input type="checkbox" checked={jabbed} onChange={onChangeJabbed} />
+                {!jabbed && <button onClick={onClickJabbed}>jab</button>}
                 <div className='initials'>{initials}</div>
                 <div>{firstName} {lastName}</div>
             </div>
